be6749c13a439d2c9e420aad521a77febf67616d
tienson86 <tienson2008@gmail.com>
2026-07-12 11:00:54 +0700

    Create __init__.py

---
+++ interpretation_engine/__init__.py
@@ -0,0 +1,18 @@
+"""
+Interpretation Engine
+
+Module xử lý:
+- đọc rule CSV
+- kiểm tra điều kiện
+- chấm điểm
+- xây dựng luận giải
+- sinh câu văn
+
+"""
+
+from .engine import InterpretationEngine
+
+
+__all__ = [
+    "InterpretationEngine"
+]
