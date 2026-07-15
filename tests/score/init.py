eec9786d6f3333c64a34c7f5a41ff81662d6af33
tienson86 <tienson2008@gmail.com>
2026-07-15 22:17:48 +0700

    Create init.py

---
+++ tests/score/init.py
@@ -0,0 +1,19 @@
+"""
+Unit Test cho Score Engine.
+"""
+
+__all__ = [
+    "test_loader",
+    "test_matcher",
+    "test_scorer",
+    "test_normalizer",
+    "test_validator",
+    "test_wuxing",
+    "test_strength",
+    "test_ten_gods",
+    "test_pattern",
+    "test_useful_god",
+    "test_shensha",
+    "test_luck",
+    "test_score_engine",
+]
