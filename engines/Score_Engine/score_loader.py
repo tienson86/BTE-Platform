b152e2c58db85453f6bdaf01a934f2cbdf850488
tienson86 <tienson2008@gmail.com>
2026-07-15 18:51:12 +0700

    Update score_loader.py

--- engines/Score_Engine/score_loader.py
+++ engines/Score_Engine/score_loader.py
@@ -1,1 +1,32 @@
+from pathlib import Path
 
+import pandas as pd
+
+
+class ScoreLoader:
+    """
+    Đọc toàn bộ dữ liệu Score Database.
+    """
+
+    def __init__(self, database_path):
+
+        self.database_path = Path(database_path)
+
+        self.cache = {}
+
+    def load_csv(self, relative_path):
+
+        path = self.database_path / relative_path
+
+        if relative_path in self.cache:
+            return self.cache[relative_path]
+
+        df = pd.read_csv(path)
+
+        self.cache[relative_path] = df
+
+        return df
+
+    def clear_cache(self):
+
+        self.cache.clear()
