7e6e85b2aa3dd3622cdba96b0d24ba57077f618b
tienson86 <tienson2008@gmail.com>
2026-07-16 11:21:20 +0700

    Create cache.py

---
+++ engines/pattern_engine/utils/cache.py
@@ -0,0 +1,26 @@
+"""
+Simple Cache.
+"""
+
+
+class SimpleCache:
+
+    def __init__(self):
+
+        self._cache = {}
+
+    def get(self, key):
+
+        return self._cache.get(key)
+
+    def set(self, key, value):
+
+        self._cache[key] = value
+
+    def clear(self):
+
+        self._cache.clear()
+
+    def size(self):
+
+        return len(self._cache)
