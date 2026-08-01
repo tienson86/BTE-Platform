ce060d7b5dcb187bd935154cd85e1bd2e7fd63e5
tienson86 <tienson2008@gmail.com>
2026-08-02 00:33:39 +0700

    Create constants.py

---
+++ engines/interpretation_engine/interpreter_runtime/interpreters/useful_god/constants.py
@@ -0,0 +1,61 @@
+"""Constants for Useful God Interpreter business logic."""
+
+from __future__ import annotations
+
+USEFUL_GOD_MODULE_IDS: tuple[str, ...] = (
+    "useful_god",
+    "useful_god_engine",
+    "13_useful_god",
+)
+
+USEFUL_GOD_SECTION_TYPE = "useful_god"
+USEFUL_GOD_INTERPRETER_ID = "useful_god_interpreter"
+USEFUL_GOD_INTERPRETER_VERSION = "1.0.0"
+
+USEFUL_GOD_KEYS: tuple[str, ...] = (
+    "useful_god",
+    "dung_than",
+    "yong_shen",
+    "yongshen",
+)
+FAVORABLE_KEYS: tuple[str, ...] = (
+    "favorable_gods",
+    "favorable",
+    "favorable_god",
+    "hy_than",
+    "xi_shen",
+    "xi",
+)
+UNFAVORABLE_KEYS: tuple[str, ...] = (
+    "unfavorable_gods",
+    "unfavorable",
+    "unfavorable_god",
+    "ky_than",
+    "ji_shen",
+    "ji",
+    "avoid_god",
+)
+SUPPORTING_KEYS: tuple[str, ...] = (
+    "supporting_elements",
+    "support_elements",
+    "resource_elements",
+)
+STRENGTH_LEVEL_KEYS: tuple[str, ...] = (
+    "strength_level",
+    "than_vuong_nhuoc",
+)
+SEASON_KEYS: tuple[str, ...] = (
+    "season",
+    "season_type",
+)
+TEMPERATURE_KEYS: tuple[str, ...] = (
+    "temperature_level",
+    "temperature_type",
+    "climate_type",
+)
+PATTERN_KEYS: tuple[str, ...] = (
+    "main_pattern",
+    "pattern",
+    "final_pattern",
+    "follow_pattern",
+)
