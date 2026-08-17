11ab171a36c25b562edea5be6f0d4dac06ca41df
tienson86 <tienson2008@gmail.com>
2026-08-17 20:56:50 +0700

    Update __init__.py

--- engines/interpretation_engine/foundation/narrative/publish/__init__.py
+++ engines/interpretation_engine/foundation/narrative/publish/__init__.py
@@ -10,18 +10,36 @@ from engines.interpretation_engine.foundation.narrative.publish.constants import
     DECISION_PUBLISH,
     PUBLISHED_NARRATIVE_BUILDER_ID,
 )
+from engines.interpretation_engine.foundation.narrative.publish.editions import (
+    EDITION_APPENDIX,
+    EDITION_EXECUTIVE,
+    EDITION_PROFESSIONAL,
+    PROFESSIONAL_REPORT_PUBLISHER_ID,
+)
 from engines.interpretation_engine.foundation.narrative.publish.models import (
     EditorialMetrics,
     PublishedNarrative,
+)
+from engines.interpretation_engine.foundation.narrative.publish.professional import (
+    ProfessionalReportPublisher,
+    apply_report_edition,
+    publication_edition_of,
 )
 
 __all__ = [
     "DECISION_APPENDIX",
     "DECISION_DROP",
     "DECISION_PUBLISH",
+    "EDITION_APPENDIX",
+    "EDITION_EXECUTIVE",
+    "EDITION_PROFESSIONAL",
     "EditorialMetrics",
-    "PUBLISHED_NARRATIVE_BUILDER_ID",
+    "PROFESSIONAL_REPORT_PUBLISHER_ID",
     "PublishedNarrative",
     "PublishedNarrativeBuilder",
+    "ProfessionalReportPublisher",
+    "PUBLISHED_NARRATIVE_BUILDER_ID",
     "apply_published_narrative",
+    "apply_report_edition",
+    "publication_edition_of",
 ]
