d3f6ce9fd9ec5e2e79937cc9f3080279e9badcf4
tienson86 <tienson2008@gmail.com>
2026-08-28 22:55:18 +0700

    Update __init__.py

--- engines/report_engine/adapters/__init__.py
+++ engines/report_engine/adapters/__init__.py
@@ -5,12 +5,22 @@ from engines.report_engine.adapters.report_input_v1_adapter import (
     ReportInputV1Source,
     build_report_input_v1,
 )
+from engines.report_engine.adapters.commercial_presentation_adapter import (
+    COMMERCIAL_PRESENTATION_EMPTY,
+    CommercialPresentationAdapter,
+    CommercialPresentationModel,
+    CommercialPresentationSection,
+)
 from engines.report_engine.adapters.wp6_assembly_bridge import (
     build_report_model_from_input,
     report_input_to_interpretation_dict,
 )
 
 __all__ = [
+    "COMMERCIAL_PRESENTATION_EMPTY",
+    "CommercialPresentationAdapter",
+    "CommercialPresentationModel",
+    "CommercialPresentationSection",
     "ReportInputV1Adapter",
     "ReportInputV1Source",
     "build_report_input_v1",
