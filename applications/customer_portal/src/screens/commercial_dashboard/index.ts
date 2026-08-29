9bf5959d70ae0c6b66e795e1b5e0d06e3b03b5dd
tienson86 <tienson2008@gmail.com>
2026-08-29 22:55:08 +0700

    Update index.ts

--- applications/customer_portal/src/screens/commercial_dashboard/index.ts
+++ applications/customer_portal/src/screens/commercial_dashboard/index.ts
@@ -9,6 +9,8 @@ export { TenGodsCard } from "./TenGodsCard";
 export { PatternCard } from "./PatternCard";
 export { ShenShaCard } from "./ShenShaCard";
 export { LuckCard } from "./LuckCard";
+export { InterpretationCard } from "./InterpretationCard";
+export { ActionPlanCard } from "./ActionPlanCard";
 export { adaptIdentityHeader, isDayMasterPillar } from "./adapter";
 export { adaptOverviewCard, composeOverviewInsight, composeOverviewConclusion } from "./overviewAdapter";
 export { adaptBaziCard } from "./baziAdapter";
@@ -17,6 +19,8 @@ export { adaptTenGodsCard } from "./tenGodsAdapter";
 export { adaptPatternCard } from "./patternAdapter";
 export { adaptShenShaCard } from "./shenShaAdapter";
 export { adaptLuckCard } from "./luckAdapter";
+export { adaptInterpretationCard } from "./interpretationAdapter";
+export { adaptActionPlanCard } from "./actionPlanAdapter";
 export { OVERVIEW_VISUAL_FIXTURE } from "./overviewFixture";
 export { BAZI_VISUAL_FIXTURE } from "./baziFixture";
 export { FIVE_ELEMENTS_VISUAL_FIXTURE } from "./fiveElementsFixture";
@@ -24,6 +28,8 @@ export { TEN_GODS_VISUAL_FIXTURE } from "./tenGodsFixture";
 export { PATTERN_VISUAL_FIXTURE } from "./patternFixture";
 export { SHENSHA_VISUAL_FIXTURE } from "./shenShaFixture";
 export { LUCK_VISUAL_FIXTURE } from "./luckFixture";
+export { INTERPRETATION_VISUAL_FIXTURE } from "./interpretationFixture";
+export { ACTION_PLAN_VISUAL_FIXTURE } from "./actionPlanFixture";
 export {
   DASHBOARD_CARDS,
   RESULT_PAGE_TITLE,
@@ -34,6 +40,8 @@ export {
   PATTERN_TITLE,
   SHENSHA_TITLE,
   LUCK_TITLE,
+  INTERPRETATION_TITLE,
+  ACTION_PLAN_TITLE,
 } from "./cards";
 export type {
   IdentityHeaderView,
@@ -45,4 +53,6 @@ export type {
   PatternView,
   ShenShaView,
   LuckView,
+  InterpretationView,
+  ActionPlanView,
 } from "./types";
