f872fc9e042ccacf727cb8404ee2221776e5f51b
tienson86 <tienson2008@gmail.com>
2026-08-29 20:15:20 +0700

    Update index.ts

--- applications/customer_portal/src/screens/commercial_dashboard/index.ts
+++ applications/customer_portal/src/screens/commercial_dashboard/index.ts
@@ -7,17 +7,23 @@ export { BaziCard } from "./BaziCard";
 export { FiveElementsCard } from "./FiveElementsCard";
 export { TenGodsCard } from "./TenGodsCard";
 export { PatternCard } from "./PatternCard";
+export { ShenShaCard } from "./ShenShaCard";
+export { LuckCard } from "./LuckCard";
 export { adaptIdentityHeader, isDayMasterPillar } from "./adapter";
 export { adaptOverviewCard, composeOverviewInsight, composeOverviewConclusion } from "./overviewAdapter";
 export { adaptBaziCard } from "./baziAdapter";
 export { adaptFiveElementsCard } from "./fiveElementsAdapter";
 export { adaptTenGodsCard } from "./tenGodsAdapter";
 export { adaptPatternCard } from "./patternAdapter";
+export { adaptShenShaCard } from "./shenShaAdapter";
+export { adaptLuckCard } from "./luckAdapter";
 export { OVERVIEW_VISUAL_FIXTURE } from "./overviewFixture";
 export { BAZI_VISUAL_FIXTURE } from "./baziFixture";
 export { FIVE_ELEMENTS_VISUAL_FIXTURE } from "./fiveElementsFixture";
 export { TEN_GODS_VISUAL_FIXTURE } from "./tenGodsFixture";
 export { PATTERN_VISUAL_FIXTURE } from "./patternFixture";
+export { SHENSHA_VISUAL_FIXTURE } from "./shenShaFixture";
+export { LUCK_VISUAL_FIXTURE } from "./luckFixture";
 export {
   DASHBOARD_CARDS,
   RESULT_PAGE_TITLE,
@@ -26,6 +32,8 @@ export {
   FIVE_ELEMENTS_TITLE,
   TEN_GODS_TITLE,
   PATTERN_TITLE,
+  SHENSHA_TITLE,
+  LUCK_TITLE,
 } from "./cards";
 export type {
   IdentityHeaderView,
@@ -35,4 +43,6 @@ export type {
   FiveElementsView,
   TenGodsView,
   PatternView,
+  ShenShaView,
+  LuckView,
 } from "./types";
