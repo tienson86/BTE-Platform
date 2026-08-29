4c79bde291a757c3ec34fb64e3a34b75a72aec95
tienson86 <tienson2008@gmail.com>
2026-08-29 22:46:22 +0700

    Update ui09_shensha.test.tsx

--- applications/customer_portal/tests/js/ui09_shensha.test.tsx
+++ applications/customer_portal/tests/js/ui09_shensha.test.tsx
@@ -196,7 +196,7 @@ describe("UI-09 ShenSha card", () => {
     const skeletons = [...container.querySelectorAll("[data-card][data-skeleton='true']")].map(
       (node) => node.getAttribute("data-card"),
     );
-    expect(skeletons).toEqual(["interpretation", "action-plan"]);
+    expect(skeletons).toEqual(["action-plan"]);
   });
 
   it("S18 expand/collapse is an accessible button", () => {
