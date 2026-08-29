ba2c7fbdded3ab9f60a5f2402ce53cfa0e875298
tienson86 <tienson2008@gmail.com>
2026-08-29 19:48:51 +0700

    Update index.ts

--- applications/customer_portal/src/screens/commercial_dashboard/index.ts
+++ applications/customer_portal/src/screens/commercial_dashboard/index.ts
@@ -6,15 +6,18 @@ export { OverviewCard } from "./OverviewCard";
 export { BaziCard } from "./BaziCard";
 export { FiveElementsCard } from "./FiveElementsCard";
 export { TenGodsCard } from "./TenGodsCard";
+export { PatternCard } from "./PatternCard";
 export { adaptIdentityHeader, isDayMasterPillar } from "./adapter";
 export { adaptOverviewCard, composeOverviewInsight, composeOverviewConclusion } from "./overviewAdapter";
 export { adaptBaziCard } from "./baziAdapter";
 export { adaptFiveElementsCard } from "./fiveElementsAdapter";
 export { adaptTenGodsCard } from "./tenGodsAdapter";
+export { adaptPatternCard } from "./patternAdapter";
 export { OVERVIEW_VISUAL_FIXTURE } from "./overviewFixture";
 export { BAZI_VISUAL_FIXTURE } from "./baziFixture";
 export { FIVE_ELEMENTS_VISUAL_FIXTURE } from "./fiveElementsFixture";
 export { TEN_GODS_VISUAL_FIXTURE } from "./tenGodsFixture";
+export { PATTERN_VISUAL_FIXTURE } from "./patternFixture";
 export {
   DASHBOARD_CARDS,
   RESULT_PAGE_TITLE,
@@ -22,6 +25,7 @@ export {
   BAZI_TITLE,
   FIVE_ELEMENTS_TITLE,
   TEN_GODS_TITLE,
+  PATTERN_TITLE,
 } from "./cards";
 export type {
   IdentityHeaderView,
@@ -30,4 +34,5 @@ export type {
   BaziStructureView,
   FiveElementsView,
   TenGodsView,
+  PatternView,
 } from "./types";
