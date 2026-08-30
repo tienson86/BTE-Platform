fa43f71ea8ae9bc3e57d834bf5fbcab8bd45a8fd
tienson86 <tienson2008@gmail.com>
2026-08-30 17:32:07 +0700

    Create index.ts

---
+++ applications/customer_portal/src/screens/executive_report/print/index.ts
@@ -0,0 +1,7 @@
+export { PrintCover } from "./PrintCover";
+export { PrintHeader } from "./PrintHeader";
+export { PrintFooter } from "./PrintFooter";
+export { PrintSection } from "./PrintSection";
+export { PrintCallout } from "./PrintCallout";
+export { PrintDivider } from "./PrintDivider";
+export { PrintAppendix } from "./PrintAppendix";
