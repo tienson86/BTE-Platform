871a700184125121813dc779960f4a5ba5efc276
tienson86 <tienson2008@gmail.com>
2026-08-30 08:16:12 +0700

    Update test_knowledge_runtime_integration.py

--- tests/narrative_v2/test_knowledge_runtime_integration.py
+++ tests/narrative_v2/test_knowledge_runtime_integration.py
@@ -8,7 +8,9 @@ from engines.narrative_v2.knowledge import NarrativeKnowledgeContext
 from engines.narrative_v2.runtime import BUILDER_STAGES, SHADOW_MODE, NarrativeRuntime
 from engines.narrative_v2.runtime.runtime_pipeline import StageResult
 
-IMPLEMENTED = frozenset({"build_evidence", "build_reasoning", "resolve_knowledge"})
+IMPLEMENTED = frozenset(
+    {"build_evidence", "build_reasoning", "resolve_knowledge", "commercial_rewrite"}
+)
 
 
 def test_k15_runtime_resolve_knowledge_returns_context(
@@ -34,6 +36,7 @@ def test_k16_later_stages_remain_not_implemented(
     runtime.pipeline.build_evidence()
     runtime.pipeline.build_reasoning()
     runtime.pipeline.resolve_knowledge()
+    runtime.pipeline.commercial_rewrite()
     later = tuple(stage for stage in BUILDER_STAGES if stage not in IMPLEMENTED)
     for stage in later:
         output = runtime.pipeline.execute_stage(stage)
