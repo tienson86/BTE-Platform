e57d54cda5377fe714bd976b2d50f99ad943c677
tienson86 <tienson2008@gmail.com>
2026-08-30 12:21:49 +0700

    Create canXuongAdapter.ts

---
+++ applications/customer_portal/src/screens/commercial_dashboard/canXuongAdapter.ts
@@ -0,0 +1,10 @@
+/**
+ * Commercial Dashboard re-export of the canonical Cân Xương adapter.
+ */
+
+export {
+  adaptCanXuong,
+  CAN_XUONG_DETAIL_HREF,
+  CAN_XUONG_EMPTY_COPY,
+} from "../../adapters/canonicalCanXuong";
+export type { CanXuongView } from "../../adapters/canonicalCanXuong";
