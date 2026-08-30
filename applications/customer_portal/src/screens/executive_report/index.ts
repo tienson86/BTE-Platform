125ca19de5fad030d3a2d43152321a68b4752170
tienson86 <tienson2008@gmail.com>
2026-08-30 17:15:14 +0700

    Create index.ts

---
+++ applications/customer_portal/src/screens/executive_report/index.ts
@@ -0,0 +1,20 @@
+export { ExecutiveReportPage } from "./ExecutiveReportPage";
+export type { ExecutiveReportPageProps } from "./ExecutiveReportPage";
+export { buildExecutiveReportView } from "./reportModel";
+export { formatPublishedDate } from "./dateDisplay";
+export { ReportCover } from "./components/ReportCover";
+export { ReportIdentity } from "./components/ReportIdentity";
+export { ExecutiveSummarySection } from "./components/ExecutiveSummarySection";
+export { ChartSnapshotSection } from "./components/ChartSnapshotSection";
+export { KeyFindingsSection } from "./components/KeyFindingsSection";
+export { InterpretationSection } from "./components/InterpretationSection";
+export { ActionPlanSection } from "./components/ActionPlanSection";
+export { LuckSection } from "./components/LuckSection";
+export { SupportingAnalysisSection } from "./components/SupportingAnalysisSection";
+export { ReportAppendix } from "./components/ReportAppendix";
+export {
+  ReportCallout,
+  ReportDivider,
+  ReportMetric,
+  ReportSectionHeader,
+} from "./components/ReportPrimitives";
